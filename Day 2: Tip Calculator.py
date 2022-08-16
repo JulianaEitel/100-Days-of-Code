1dad9cfe04457612e4c4abebe38e88749723998b
Juliana <110264632+JulianaEitel@users.noreply.github.com>
2022-08-16 17:15:32 -0400

    Add files via upload

--- Day 2: Tip Calculator.py
+++ Day 2: Tip Calculator.py
@@ -21,6 +21,6 @@ bill = float(input("What was the total bill? $"))
 tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
 people = int(input("How many people to split the bill? "))
 bill_with_tip = ((tip / 100) * bill) + bill
-total = round(bill_with_tip / people, 2)
+total = "{:.2f}".format(bill_with_tip / people)
 
 print(f"Each person should pay: ${total}")
