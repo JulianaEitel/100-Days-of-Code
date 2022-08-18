6088310dfccd891b86b443e1ff8af2271a19c14e
Juliana <110264632+JulianaEitel@users.noreply.github.com>
2022-08-18 13:35:42 -0400

    Add files via upload

---
+++ Day 4: Rock Paper Scissors.py
@@ -0,0 +1,78 @@
+import random
+
+my_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors."))
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+#Write your code below this line 👇
+"""
+From there you will need to figure out:
+* How you will store the user's input.
+* How you will generate a random choice for the computer.
+* How you will compare the user's and the computer's choice to determine the winner (or a draw).
+* And also how you will give feedback to the player.
+"""
+if my_choice == 0:
+    print(rock)
+elif my_choice == 1:
+    print(paper)
+elif my_choice == 2:
+    print(scissors)
+
+print("Computer chose:")
+
+comp_choice = random.randint(0, 2)
+
+if comp_choice == 0:
+    print(rock)
+elif comp_choice == 1:
+    print(paper)
+elif comp_choice == 2:
+    print(scissors)
+
+
+if my_choice == 0 and comp_choice == 2:
+    print("You win!")
+elif my_choice == 0 and comp_choice == 1:
+    print("You lose!")
+
+elif my_choice == 1 and comp_choice == 0:
+    print("You win!")
+elif my_choice == 1 and comp_choice == 2:
+    print("You lose!")
+
+elif my_choice == 2 and comp_choice == 1:
+    print("You win!")
+elif my_choice == 2 and comp_choice == 0:
+    print("You lose!")
+
+elif my_choice == comp_choice:
+    print("It's a tie!")
+    
+else:
+    print("You chose an invalid number. You lose!")
