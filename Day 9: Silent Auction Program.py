54aae0fce4ee959837d4d5223f4f065664dbad06
Juliana <110264632+JulianaEitel@users.noreply.github.com>
2022-08-24 08:47:21 -0400

    Add files via upload

--- Day 9: Silent Auction Program.py
+++ Day 9: Silent Auction Program.py
@@ -1,26 +1,31 @@
 print("Welcome to the Silent Auction Program!")
 
-bids = {}
-bidding_finished = False
 
-def find_highest_bidder(bids):
+def bids_submitted(name, bid):
+  bids_record = {}
+  bids_record[name] = bid
+
+
+def highest_bid(name, bid):
   highest_bid = 0
-  winner = ""
-  for bidder in bids:
-    bid_amount = bids[bidder]
-    if bid_amount > highest_bid:
-      highest_bid = bid_amount
-      winner = bidder
-  print(f"The winner is {winner} with a bid of ${highest_bid}")
+  if bid > highest_bid:
+    highest_bid = bid
+  print(f"{name} won with a bid of ${bid}.")
 
 
-while not bidding_finished:
+start_again = True
+while start_again:
   name = input("What is your name?: ")
-  price = int(input("What is your bid?: $"))
-  bids[name] = price
-  should_continue = input("Are there any other bidders? Type 'yes' or 'no':\n").lower()
-  if should_continue == "no":
-    bidding_finished = True
-    find_highest_bidder(bids)
+  bid = int(input("What is your bid?: "))
 
+  bids_submitted(name, bid)
+
+  other_bidder = input("Are there any other bidders? Type 'yes' or 'no'.\n").lower()
+
+  if other_bidder == "yes":
+    start_again = True
+
+  elif other_bidder == "no":
+    start_again = False
+    highest_bid(name, bid)
 
