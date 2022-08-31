00b283b34a360962520bb860b740a0459ac3aaf4
Juliana <110264632+JulianaEitel@users.noreply.github.com>
2022-08-31 15:23:22 -0400

    Add files via upload

--- Day 10: Calculator.py
+++ Day 10: Calculator.py
@@ -1,44 +1,53 @@
-def calculation():
-    def add(n1, n2):
-        return n1 + n2
+def add(n1, n2):
+    return n1 + n2
 
-    def subtract(n1, n2):
-        return n1 - n2
 
-    def multiply(n1, n2):
-        return n1 * n2
+def subtract(n1, n2):
+    return n1 - n2
 
-    def divide(n1, n2):
-        return n1 / n2
 
-    operations = {
-        "+": add,
-        "-": subtract,
-        "*": multiply,
-        "/": divide,
-    }
-    num1 = float(input("Enter a number: "))
+def multiply(n1, n2):
+    return n1 * n2
+
+
+def divide(n1, n2):
+    return n1 / n2
+
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+}
+
+
+def calculator():
+    n1 = int(input("What's the first number?: "))
+
+    for symbol in operations:
+        print(symbol)
+
     should_continue = True
     while should_continue:
-        for symbol in operations:
-            print(symbol)
-        operation_symbol = input("Enter an operation from the list above: ")
-        num2 = float(input("Enter a number: "))
 
-        calculation_function = operations[operation_symbol]
-        answer = calculation_function(num1, num2)
+        operation_symbol = input("Pick an operation from the list above: ")
+        n2 = int(input("What's the next number?: "))
 
-        print(f"{num1} {operation_symbol} {num2} = {answer}")
+        operation_function = operations[operation_symbol]
+        answer = operation_function(n1, n2)
 
-        restart = input(
-            f"Type 'y' if you want to continue with {answer}, otherwise type 'n' to start a new calculation: ")
-        if restart == "y":
-            num1 = answer
-            should_continue = True
+        print(f"{n1} {operation_symbol} {n2} = {answer}")
 
-        elif restart == "n":
+        continue_calculating = input(
+            f"Type 'y' to continue with {answer}, otherwise type 'n' to start a new calculation: ")
+
+        if continue_calculating == 'y':
+            n1 = answer
+
+        else:
             should_continue = False
-            calculation()
+            calculator()
 
 
-calculation()
+calculator()
